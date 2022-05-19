8082c7840884fadd4d82a608d9c1dc4a47730719
kazukichi <20309063+n-kazukichi@users.noreply.github.com>
2022-05-19 18:08:09 +0900

    次回ビルドの速度確認

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
+      <p>Hello!! React</p>
       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
